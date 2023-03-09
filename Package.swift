ba085cd4dff58868ec2edabe4669a35cef703077
genexusbot <builder@genexus.com>
2023-03-09 06:36:18 -0300

    Package version: 1.0.0-beta+20230309092859

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Synchronization",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Synchronization-1.0.0-beta+20230307184249.xcframework.zip",
-			checksum: "d4955301959752902c43b39cecaf258020b956f1d1f52bdd08c12cd6b679b8ea"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Synchronization-1.0.0-beta+20230309092859.xcframework.zip",
+			checksum: "909c7e047d3085b5e75498c20e44561107de511a4b30a143d958f5d2b769a243"
 		)
 	]
 )
