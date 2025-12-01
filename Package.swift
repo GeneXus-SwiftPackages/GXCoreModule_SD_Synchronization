227fadf867fb1870d59544655b5c3982fc8928b6
The iOS Team @ GeneXus <builder@genexus.com>
2025-12-01 06:44:07 -0500

    Package version: 4.1.0-beta.13

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_SynchronizationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.1.0-beta.12")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.1.0-beta.13")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_SynchronizationWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Synchronization",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Synchronization-4.1.0-beta.12.xcframework.zip",
-			checksum: "6b403a4170f91030d02c89e39a49ebe6f63fc0d8e96384a5b8d933648cf61a63"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Synchronization-4.1.0-beta.13.xcframework.zip",
+			checksum: "0c095991685b7625b13179d78db9461316d45134dda97a371b1408e337aceb35"
 		)
 	]
 )
